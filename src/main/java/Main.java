715098871812e24aeeec0c215183817b0c16c3ed
mpalmer45 <94503201+mpalmer45@users.noreply.github.com>
2021-11-17 18:35:06 +1030

    Beginning work on the main class/method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,8 +22,26 @@ public class Main {
         // check the file exists
         File file = new File(filename);
         if(!file.exists()) {
-            System.out.println("File does not exist");
+            System.out.println("File does not exist.");
             return;
         }
+
+        // attempt to decode the CSV into a list of arrays
+        List<String[]> decodedCSV = new ArrayList<>();
+        try {
+            decodedCSV = CSVHelper.readCsv(file);
+        } catch (IOException e) {
+            Logger.error("There was an exception while decoding the CSV - " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        // check if something is in the decoded csv
+        if(decodedCSV.size() == 0) {
+            System.out.println("CSV file appears empty.");
+            return;
+        }
+
+        // convert the csv data to players
+        List<Player> players = Player.getPlayersFromCSV(decodedCSV);
     }
 }
